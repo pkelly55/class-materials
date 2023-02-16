1edd10cb6e3a3d2a06d70ea0d7abc7cb13b0eace
Peyton Kelly <pkelly5511@gmail.com>
2023-02-16 09:07:28 -0500

    Update note.py

--- Week4/note.py
+++ Week4/note.py
@@ -2,6 +2,7 @@
 #output = sorted array of numbers (integers)
 # expected output = [1,2,3,4,5,6,7]
 # Insertion algorithm
+'''
 def sort(input):
     for i in range(len(input)):
         key = input[i]
@@ -14,10 +15,34 @@ def sort(input):
         
    
     return input
-     
+'''
+# partition(a,p,r) algorithm 
+
+def partition(a,p,r):
+    x = a[r]
+    i = p - 1
+    for j in range(p,r):
+        if a[j] <= x:
+            i = i + 1
+            a[i],a[j] = a[j],a[i]
+    a[i + 1],a[r] = a[r],a[i + 1]
+    return i + 1
+
+# quicksort(a,p,r) algorithm
+
+def quicksort(a,p,r):
+    if p < r:
+        q = partition(a,p,r)
+        quicksort(a,p,q - 1)
+        quicksort(a,q + 1,r)
+    return a
+
+
+            
+    
 def main():
-    input = [4,5,1,3,2,7,6]
-    print(sort(input))
+    input = [4,5,1,3,2,7,6,9,8]
+    print(quicksort(input,0,len(input) - 1))
     
 if __name__ == "__main__":
     main()
